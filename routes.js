b3483e429486923165cbcf6ac09ef0a762505dc2
Adrian <adrian.tiu@obf.ateneo.edu>
2019-06-07 21:46:01 +0800

    add route to user

--- routes.js
+++ routes.js
@@ -2,9 +2,19 @@ var express = require('express')
 var router = express.Router()
 const routes = router
 
+const user = require('./user')
+
 // Home URL
 routes.get('/', function(req, res) {
     res.send('Scraper API is up and working!')
+})
+
+// Part 1
+routes.get('/user_profile/:username', function(req,res) {
+    username = req.params.username
+    user(username, (profile) => {
+        res.json(profile)
+    })
 })
 
 module.exports = router
